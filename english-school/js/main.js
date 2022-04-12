be1472575fc04bef6efb5e414326d13d5971a4bc
TomoyukiWakiyama <kreva2002@gmail.com>
2022-04-12 21:53:29 +0900

    0412 2153

--- english-school/js/main.js
+++ english-school/js/main.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const so = new ScrollObserver('.so', 'scroll-in', {rootMargin: "0px 0px -240px 0px"});
+    const so = new ScrollObserver('.so', 'scroll-in', {rootMargin: "0px 0px -200px 0px"});
+    const bl = new ScrollObserver('.review-person__inner', 'balloonReview', {rootMargin: "0px 0px -120px 0px"});
 });
